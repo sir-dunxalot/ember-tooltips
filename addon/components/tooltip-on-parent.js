f03860ce5cbe8b6a9025773de500268bff769364
Duncan Graham Walker <walkerdu@bc.edu>
2015-06-22 23:14:43 -0400

    Examples updated with new tooltipSupportedProperties

--- addon/components/tooltip-on-parent.js
+++ addon/components/tooltip-on-parent.js
@@ -2,9 +2,7 @@ import Ember from 'ember';
 import layout from '../templates/components/tooltip-on-parent';
 
 export default Ember.Component.extend({
-  attributeBindings: ['style'],
   layout: layout,
-  style: 'display:hidden;',
 
   registerOnParent: Ember.on('didInsertElement', function() {
     this.get('parentView').renderTooltip(this);
