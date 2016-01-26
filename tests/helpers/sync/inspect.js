5cb11672ea0edd1e1528cc556cfff0f15f936abf
Duncan Graham Walker <walkerdu@bc.edu>
2016-01-25 21:08:34 -0500

    Failing test for #36

--- tests/helpers/sync/inspect.js
+++ tests/helpers/sync/inspect.js
@@ -3,10 +3,7 @@ import selectorFor from '../selector-for';
 
 export default Ember.Test.registerHelper('inspect',
   function(app, name, useJquery = true) {
-    console.log('finding');
     const [ element ] = find(selectorFor(name)).toArray();
-    // const [ element ] = ;
-    console.log('running', element);
 
     if (useJquery) {
       return $(element);
