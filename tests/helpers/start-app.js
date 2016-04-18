7bba6e304683e99a05bda2937b0db9118c36075f
Matthew Dahl <mdahl@ciena.com>
2016-04-18 07:47:16 -0700

    Update test helper

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -8,11 +8,13 @@ import './async/mouse-out';
 import './async/mouse-over';
 import './sync/inspect';
 
+const assign = Ember.assign || Ember.merge;
+
 export default function startApp(attrs) {
   let application;
 
-  let attributes = Ember.merge({}, config.APP);
-  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
+  let attributes = assign({}, config.APP);
+  attributes = assign(attributes, attrs); // use defaults, but you can override;
 
   Ember.run(() => {
     application = Application.create(attributes);
