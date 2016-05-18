2595939074075bcd8bcaa1f050d2160a2849873d
Jonathan Lepolt <lepolt@batterii.com>
2016-05-18 10:56:59 -0400

    Fix issue where using bindings with SafeStrings would crash the Tooltip library. Added tests.

--- tests/acceptance/tooltip-with-safestring-test.js
+++ tests/acceptance/tooltip-with-safestring-test.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import { module, test } from 'qunit';
 import startApp from '../helpers/start-app';
+import selectorFor from '../helpers/selector-for';
 
 let application;
 
@@ -22,6 +23,27 @@ test('Rendering safestring works', function(assert) {
 
   assertTooltipProperties(assert, 'on-inline-component', {
     content: 'this is a test',
+  });
+
+});
+
+test('Binding to property that returns different safestrings works', function(assert) {
+
+  visit('/tooltip-with-safestring');
+
+  assert.expect(12);
+
+  assertTooltipProperties(assert, 'toggle-safestrings', {
+    content: 'SafeString 1',
+  });
+
+  // Clicking button will toggle the text
+  click(selectorFor('button'));
+
+  andThen(function() {
+    assertTooltipProperties(assert, 'toggle-safestrings', {
+      content: 'SafeString 2',
+    });
   });
 
 });
--- tests/dummy/app/controllers/tooltip-with-safestring.js
+++ tests/dummy/app/controllers/tooltip-with-safestring.js
@@ -6,8 +6,26 @@ export default Ember.Controller.extend({
     debugging() {
       this.transitionToRoute('index');
     },
+
+    toggle() {
+      this.toggleProperty('toggle');
+    },
   },
 
   safeString: new Ember.Handlebars.SafeString('this is a test'),
 
+  toggle: true,
+
+  safeStringToggle: Ember.computed('toggle', function() {
+    let toggle = this.get('toggle');
+    let safeString;
+
+    if (toggle) {
+      safeString = new Ember.Handlebars.SafeString('SafeString 1');
+    } else {
+      safeString = new Ember.Handlebars.SafeString('SafeString 2');
+    }
+
+    return safeString;
+  }),
 });
