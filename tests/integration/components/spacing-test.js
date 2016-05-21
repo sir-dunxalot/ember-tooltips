e93c16466e460b6901d71bbef42574f45a887de3
Duncan Graham Walker <walkerdu@bc.edu>
2016-05-21 17:11:55 -0400

    Test added for spacing

---
+++ tests/integration/components/spacing-test.js
@@ -0,0 +1,39 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+function assertSpacing(assert, context, expectedSpacing) {
+  const $this = context.$();
+  const targetPosition = $this.position();
+  const $tooltip = $this.find('.tooltip');
+  const tooltipPosition = $tooltip.position();
+  const offset = Math.floor(targetPosition.top - tooltipPosition.top);
+
+  const paddingTop = parseInt($tooltip.css('padding-top'));
+  const paddingBottom = parseInt($tooltip.css('padding-bottom'));
+
+  assert.equal(offset - paddingTop - paddingBottom, expectedSpacing,
+    `Tooltip should be ${expectedSpacing}px from the target`);
+
+}
+
+moduleForComponent('tooltip-on-component', 'Integration | Option | spacing', {
+  integration: true
+});
+
+test('It shows with showOn', function(assert) {
+
+  assert.expect(2);
+
+  /* Check the default spacing */
+
+  this.render(hbs`{{tooltip-on-component}}`);
+
+  assertSpacing(assert, this, 10);
+
+  /* Check custom spacing */
+
+  this.render(hbs`{{tooltip-on-component spacing=20}}`);
+
+  assertSpacing(assert, this, 20);
+
+});
