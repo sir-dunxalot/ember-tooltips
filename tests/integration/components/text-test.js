b16847edb0e87809d9ac9491009c48a74c7d559f
a15n <andrewscheuermann@gmail.com>
2016-12-10 13:43:02 -0800

    use assertTooltipRendered in text= test

--- tests/integration/components/text-test.js
+++ tests/integration/components/text-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { assertTooltipRendered } from '../../helpers/ember-tooltips';
 
 moduleForComponent('tooltip-on-element', 'Integration | Component | inline', {
   integration: true
@@ -7,14 +8,17 @@ moduleForComponent('tooltip-on-element', 'Integration | Component | inline', {
 
 test('tooltip-on-element renders with text param', function(assert) {
 
+	assert.expect(2);
+
   this.render(hbs`
     {{tooltip-on-element text='Here is more info'}}
   `);
 
+  const $body = this.$().parents('body');
+
   assert.equal(this.$().text().trim(), 'Here is more info',
     'Should render with content equal to the text property');
 
-  assert.ok(this.$().find('.ember-tooltip').length,
-    'Should create a tooltip element');
+  assertTooltipRendered($body, assert);
 
 });
