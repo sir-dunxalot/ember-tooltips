351d606e43cd99047a8a80b59097b2ebeeec910b
a15n <andrewscheuermann@gmail.com>
2016-12-15 07:31:49 -0800

    add LTS 2.4 and 2.8 support

--- config/ember-try.js
+++ config/ember-try.js
@@ -19,6 +19,28 @@ module.exports = {
       }
     },
     {
+      name: 'ember-lts-2.4',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#lts-2-4'
+        },
+        resolutions: {
+          'ember': 'lts-2-4'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.8',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#lts-2-8'
+        },
+        resolutions: {
+          'ember': 'lts-2-8'
+        }
+      }
+    },
+    {
       name: 'ember-release',
       bower: {
         dependencies: {
