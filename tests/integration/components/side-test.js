093d1ecbd2466da1fafddc14a487104cea82d397
Duncan Graham Walker <walkerdu@bc.edu>
2016-12-20 21:33:47 +0000

    Position assertion moved to test helpers file

--- tests/integration/components/side-test.js
+++ tests/integration/components/side-test.js
@@ -1,26 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-function assertPosition(assert, context, expectedSide) {
-  const $this = context.$();
-  const targetPosition = $this.position();
-  const tooltipPosition = $this.find('.ember-tooltip').position();
-
-  if (expectedSide === 'top') {
-    assert.ok(targetPosition.top > tooltipPosition.top,
-      'Tooltip should be above the target');
-  } else if (expectedSide === 'right') {
-    assert.ok(targetPosition.left < tooltipPosition.left,
-      'Tooltip should be right of the target');
-  } else if (expectedSide === 'bottom') {
-    assert.ok(targetPosition.top < tooltipPosition.top,
-      'Tooltip should be below the target');
-  } else if (expectedSide === 'left') {
-    assert.ok(targetPosition.left > tooltipPosition.left,
-      'Tooltip should be left of the target');
-  }
-}
-
 moduleForComponent('tooltip-on-element', 'Integration | Option | side and keepInWindow', {
   integration: true
 });
