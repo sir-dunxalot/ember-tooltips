3d909cfd73b5f39c958026929b8eb348a536db29
Duncan Graham Walker <walkerdu@bc.edu>
2017-01-08 21:51:51 -0500

    Remove Ember 1.13 from tests

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -36,7 +36,7 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
+    ENV.rootURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
@@ -48,8 +48,8 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.baseURL = '/ember-tooltips/';
-    ENV.baseURL = '/ember-tooltips';
+    ENV.rootURL = '/ember-tooltips/';
+    ENV.rootURL = '/ember-tooltips';
   }
 
   return ENV;
