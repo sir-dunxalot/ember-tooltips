aa2bc5fb76d18a8680048077973802b6f9f8fd2f
Glenn Vandamme <glenn.vd@gmail.com>
2017-02-07 14:33:13 +0100

    Update tether-tooltip-and-popover.js

--- addon/components/tether-tooltip-and-popover.js
+++ addon/components/tether-tooltip-and-popover.js
@@ -216,7 +216,7 @@ export default EmberTetherComponent.extend({
       return null;
     }
 
-    const parentElement = $element.parent();
+    const parentElement = $element.parent().parent();
     let parentElementId = parentElement && parentElement.attr('id');
 
     if (!parentElementId) {
