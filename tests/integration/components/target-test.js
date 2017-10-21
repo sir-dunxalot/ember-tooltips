683338b31f1407645e75cd57d13dd73389485a2c
Duncan Graham Walker <walkerdu@bc.edu>
2017-10-21 14:50:03 -0400

    More comprehensive target test passing

--- tests/integration/components/target-test.js
+++ tests/integration/components/target-test.js
@@ -1,5 +1,11 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import {
+  afterTooltipRenderChange,
+  findTooltip,
+  findTooltipTarget,
+  triggerTooltipTargetEvent,
+} from 'dummy/tests/helpers/ember-tooltips';
 
 moduleForComponent('ember-tooltip', 'Integration | Component | target', {
   integration: true,
@@ -7,14 +13,33 @@ moduleForComponent('ember-tooltip', 'Integration | Component | target', {
 
 test('ember-tooltip target test', function(assert) {
 
+  assert.expect(4);
+
   this.render(hbs`
     <div id="some-target"></div>
-    {{ember-tooltip target="#some-target"}}
+    {{ember-tooltip targetId='some-target'}}
   `);
 
-  const $someTarget = this.$().find('#some-target');
+  const expectedTarget = this.$().find('#some-target');
+  const actualTarget = findTooltipTarget();
 
-  assert.ok($someTarget.hasClass('ember-tooltip-or-popover-target'),
+  assert.ok(expectedTarget.hasClass('ember-tooltip-target'),
       '#some-target should be the tooltip target');
 
+  assert.equal(expectedTarget[0], actualTarget[0],
+    'The element with ID equal to targetID should be the tooltip target');
+
+  triggerTooltipTargetEvent(actualTarget, 'mouseenter');
+
+  afterTooltipRenderChange(assert, () => {
+    const tooltip = findTooltip();
+    const targetDescribedby = actualTarget.attr('aria-describedby');
+
+    assert.ok(!!targetDescribedby,
+      'The target should have an aria-describedby attribute after the tooltip renders');
+
+    assert.equal(targetDescribedby, tooltip.attr('id'),
+      `The tooltip ID should match the target's aria-describedby attribute`);
+
+  });
 });
