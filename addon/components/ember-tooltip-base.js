e1a30551c4a875218318483a2eb8e756f223a463
mfees <maddie.fees@izea.com>
2018-04-18 16:23:43 -0400

    set preventOverflow.escapeWithReference to true on popper modifiers

--- addon/components/ember-tooltip-base.js
+++ addon/components/ember-tooltip-base.js
@@ -317,6 +317,9 @@ export default Component.extend({
                 flip: {
                   enabled: this.get('enableFlip'),
                 },
+                preventOverflow: {
+                  escapeWithReference: true
+                }
               },
 
               onCreate: (tooltipData) => {
