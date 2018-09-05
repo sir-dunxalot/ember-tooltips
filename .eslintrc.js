1713cd334455019de69f46eba7d1f4b8eee9f087
Max Fierke <max@maxfierke.com>
2018-09-04 20:52:07 -0500

    ember-cli-update

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,13 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module',
   },
+  plugins: [
+    'ember'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended'
+  ],
   env: {
     browser: true,
   },
@@ -15,46 +22,36 @@ module.exports = {
     'netguru-ember', // Import rules but don't automatically add the config
   ],
   rules: {
-    'block-spacing': 'error',
-    'brace-style': ['error', '1tbs', {
-      'allowSingleLine': true,
-    }],
-    'capitalized-comments': ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'eol-last': ['error', 'always'],
-    'func-call-spacing': ['error', 'never'],
-    'func-style': ['error', 'declaration', {
-      'allowArrowFunctions': true,
-    }],
-    'line-comment-position': ['error', {
-      'position': 'beside',
-    }],
-    'lines-around-comment': ['error', {
-      'afterBlockComment': true,
-      'beforeBlockComment': true,
-    }],
-    'newline-after-var': ['error', 'always'],
-    'newline-before-return': 'error',
-    'no-trailing-spaces': 'error',
-    'object-property-newline': 'error',
-    'one-var': ['error', 'never'],
-    'space-before-blocks': 'error',
-
-    /* ember-suave custom rules
-
-    https://github.com/DockYard/eslint-plugin-ember-suave/tree/master/docs/rules
-    */
-
-    'ember-suave/no-const-outside-module-scope': 'off',
-
-    /* netguru-ember rules
-
-    https://github.com/netguru/eslint-plugin-netguru-ember
-    */
-
-    'netguru-ember/jquery-ember-run': 1,
-    'netguru-ember/no-function-prototype-extensions': 1,
-    'netguru-ember/order-in-components': 1,
-    'netguru-ember/no-on-calls-in-components': 1,
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        'ember-cli-build.js',
+        'index.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js'
+      ],
+      excludedFiles: [
+        'addon/**',
+        'addon-test-support/**',
+        'app/**',
+        'tests/dummy/app/**'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+      })
+    }
+  ]
 };
