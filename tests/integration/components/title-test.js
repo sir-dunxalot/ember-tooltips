6006f2107cf7fb26676bc13776423c8cd54f2b0e
Duncan Walker <walkerdu@bc.edu>
2018-10-08 22:52:07 -0400

    Added test coverage for title when hiding and showing tooltip

--- tests/integration/components/title-test.js
+++ tests/integration/components/title-test.js
@@ -1,10 +1,11 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, triggerEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import {
-  assertTooltipRendered,
   assertTooltipContent,
+  assertTooltipNotVisible,
+  assertTooltipVisible,
   findTooltipTarget,
 } from 'ember-tooltips/test-support';
 
@@ -12,21 +13,37 @@ module('Integration | Component | title', function(hooks) {
   setupRenderingTest(hooks);
 
   test(`ember-tooltip uses text instead of parent's title attribute`, async function(assert) {
-    assert.expect(3);
+    assert.expect(5);
 
     await render(hbs`
       <div title="Hover for more info">
-        {{ember-tooltip text='Here is more info' isShown=true}}
+        {{ember-tooltip text='Here is more info'}}
       </div>
     `);
 
-    assertTooltipRendered(assert);
+    const [ target ] = findTooltipTarget();
+
+    await triggerEvent(target, 'mouseenter');
+
+    assertTooltipVisible(assert);
 
     assertTooltipContent(assert, {
       contentString: 'Here is more info',
     });
 
-    assert.equal(findTooltipTarget().attr('title'), 'Hover for more info',
+    /* Reshow the tooltip and check it's content is still correct */
+
+    await triggerEvent(target, 'mouseleave');
+
+    assertTooltipNotVisible(assert);
+
+    await triggerEvent(target, 'mouseenter');
+
+    assertTooltipContent(assert, {
+      contentString: 'Here is more info',
+    });
+
+    assert.equal(target.title, 'Hover for more info',
       'The target should have the original title');
   });
 });
