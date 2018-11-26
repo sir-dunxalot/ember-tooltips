c6f737de9f9c653b0ce7847dc6ed9520a344354c
arthur5005 <arthur5005@gmail.com>
2018-11-26 10:29:42 -0800

    Fix issue hide/show race by cancelling the final hide callback on show
    Added a _completeHideTimer to track the final run.later which actually completes the hiding of the tooltip. This is different than the _hideTimer which tracks a desired hide of the tooltip based on a duration supplied by the user.
    
    This timer gets cancelled on every show to prevent a race condition between showing and hiding the tooltip.

---
+++ tests/integration/components/show-hide-show-test.js
@@ -0,0 +1,28 @@
+import { later } from '@ember/runloop';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, triggerEvent } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import {
+  assertTooltipNotRendered,
+  assertTooltipVisible,
+} from 'ember-tooltips/test-support';
+
+module('Integration | Component | show-hide-show', function(hooks) {
+  setupRenderingTest(hooks);
+
+  //Prevents a race condition regression between hiding and showing the tooltip when the user moves their mouse over quickly.
+  test('ember-tooltip shows after quickly showing then hiding then showing again.', async function(assert) {
+    assert.expect(2);
+
+    await render(hbs`{{ember-tooltip}}`);
+
+    assertTooltipNotRendered(assert);
+    triggerEvent(this.element, 'mouseenter');
+    triggerEvent(this.element, 'mouseleave');
+    triggerEvent(this.element, 'mouseenter');
+
+    //Wait at least 200 ms (animation time) for a hide to potentially fire then check for visibility. Should still be visible.
+    later(() => assertTooltipVisible(assert), 250);
+  });
+});
