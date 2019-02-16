sir-dunxalot/ember-tooltips

commit b83580c43689d922cb53e002479781332f9be4ef

Fix bug where bound text could not be updated
Removes workaround for ember-wormhole@0.5.4 renderInPlace
binding issue and ups runtime dependency to 0.5.5.

Fixes #225

addon/components/ember-tooltip-base.js
@@ -300,12 +300,11 @@ export default Component.extend({
           const rootElement = document.querySelector(config.APP.rootElement);
           const target = this.get('target');
           const tooltipClassName = this.get('tooltipClassName');
-          const tooltipContent = this.get('text') || '<span></span>';
           const tooltip = new Tooltip(target, {
             container: rootElement || false,
             html: true,
             placement: this.get('side'),
-            title: tooltipContent,
+            title: '<span></span>',
             trigger: 'manual',
             template: `<div class="tooltip ${tooltipClassName} ember-tooltip-effect-${this.get('effect')}" role="tooltip" style="margin-${getOppositeSide(this.get('side'))}:${this.get('spacing')}px;">
                         <div class="tooltip-arrow ember-tooltip-arrow"></div>

addon/templates/components/ember-tooltip-base.hbs
@@ -1,12 +1,9 @@
-{{#if hasBlock}}
-  {{#if _tooltipElementRendered}}
-    {{#ember-wormhole
-      to=wormholeId
-      renderInPlace=tooltipElementNotRendered
-    }}
-      <div>
-        {{yield this}}
-      </div>
-    {{/ember-wormhole}}
-  {{/if}}
-{{/if}}
+{{#ember-wormhole to=wormholeId renderInPlace=tooltipElementNotRendered}}
+  <div>
+    {{#if (hasBlock)}}
+      {{yield this}}
+    {{else}}
+      {{text}}
+    {{/if}}
+  </div>
+{{/ember-wormhole}}

tests/integration/components/text-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, settled } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import {
   assertTooltipRendered,
@@ -21,6 +21,28 @@ module('Integration | Component | inline', function(hooks) {
 
     assertTooltipContent(assert, {
       contentString: 'Here is more info',
+    });
+  });
+
+  test('ember-tooltip updates when text param changes', async function(assert) {
+    assert.expect(3);
+
+    this.set('tooltipText', 'Here is more info');
+    await render(hbs`
+      {{ember-tooltip text=tooltipText isShown=true}}
+    `);
+
+    assertTooltipRendered(assert);
+
+    assertTooltipContent(assert, {
+      contentString: 'Here is more info',
+    });
+
+    this.set('tooltipText', 'Here is the latest info');
+    await settled();
+
+    assertTooltipContent(assert, {
+      contentString: 'Here is the latest info',
     });
   });
 });

commit 60c67bccfa85203c28ee88efd2ce4dafd6dd06e9

Avoid rendering content into DOM until popper element created
Fixes #341 and more adequately addresses concerns from #340, #330
without requiring the user to check `isShown`. Bug was introduced
by me in b83580c43689d922cb53e002479781332f9be4ef, and broke a
promise about how enableLazyRendering in 2.x would essentially be
the default and behave the same way.

However, the fastboot tests established that the content would be
rendered into the DOM in fastboot mode, so this preserves that
behavior.

addon/components/ember-tooltip-base.js
@@ -1,5 +1,6 @@
 import Tooltip from 'tooltip.js';
 import Ember from 'ember';
+import { getOwner } from '@ember/application';
 import { computed } from '@ember/object';
 import { assign } from '@ember/polyfills';
 import { run } from '@ember/runloop';
@@ -157,6 +158,19 @@ export default Component.extend({
 
   wormholeId: computed('elementId', function() {
     return `${this.get('elementId')}-wormhole`;
+  }),
+
+  _fastboot: computed(function() {
+    let owner = getOwner(this);
+    return owner.lookup('service:fastboot');
+  }),
+
+  _shouldRenderContent: computed(
+    '_fastboot.isFastBoot',
+    '_awaitingTooltipElementRendered',
+    function() {
+    return this.get('_fastboot.isFastBoot') ||
+      !this.get('_awaitingTooltipElementRendered');
   }),
 
   _awaitingTooltipElementRendered: true,

addon/templates/components/ember-tooltip-base.hbs
@@ -1,9 +1,11 @@
 {{#ember-wormhole to=wormholeId renderInPlace=_awaitingTooltipElementRendered}}
   <div>
-    {{#if (hasBlock)}}
-      {{yield this}}
-    {{else}}
-      {{text}}
+    {{#if _shouldRenderContent}}
+      {{#if (hasBlock)}}
+        {{yield this}}
+      {{else}}
+        {{text}}
+      {{/if}}
     {{/if}}
   </div>
 {{/ember-wormhole}}