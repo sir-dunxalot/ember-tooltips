292cfa1641de23f312bcc0bac65b61299461eff5
Max Fierke <max@maxfierke.com>
2020-01-18 22:00:37 -0600

    Upgrade to Ember 3.15

--- config/ember-try.js
+++ config/ember-try.js
@@ -35,6 +35,14 @@ module.exports = async function() {
         }
       },
       {
+        name: 'ember-lts-3.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.12.0'
+          }
+        }
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
@@ -78,6 +86,21 @@ module.exports = async function() {
         npm: {
           devDependencies: {
             '@ember/jquery': '^0.5.1'
+          }
+        }
+      },
+      {
+        name: 'ember-classic',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'application-template-wrapper': true,
+            'default-async-observers': false,
+            'template-only-glimmer-components': false
+          })
+        },
+        npm: {
+          ember: {
+            edition: 'classic'
           }
         }
       }
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,15 +1,13 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL,
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function() {
   this.route('acceptance');
   this.route('many-tooltips');
   this.route('update-for-reposition');
 });
-
-export default Router;
