2bff5bf7c7af4842fd5004b27662d8f54dad6a33
Max Fierke <max@maxfierke.com>
2020-01-20 17:32:20 -0600

    Fix broken Ember.assert imports
    The babel plugin that does this transforms doesn't support
    aliasing, so these were just importing undefineds.

--- addon-test-support/dom/assertions/assert-tooltip-visible.js
+++ addon-test-support/dom/assertions/assert-tooltip-visible.js
@@ -1,11 +1,17 @@
+import { assert } from '@ember/debug';
 import { findTooltip } from 'ember-tooltips/test-support/dom';
 
-export default function assertTooltipVisible(assert, options = {}) {
+export default function assertTooltipVisible(qunitAssert, options = {}) {
   const { selector } = options;
   const tooltip = findTooltip(selector, options);
+
+  if (!tooltip) {
+    assert(`assertTooltipVisible(): Could not find a tooltip for selector: ${selector}`);
+  }
+
   const ariaHidden = tooltip.getAttribute('aria-hidden');
 
-  assert.ok(ariaHidden === 'false',
+  qunitAssert.ok(ariaHidden === 'false',
     `assertTooltipVisible(): the ember-tooltip should be visible:
       aria-hidden = ${ariaHidden}`);
 }
--- addon-test-support/jquery/assertions/assert-tooltip-spacing.js
+++ addon-test-support/jquery/assertions/assert-tooltip-spacing.js
@@ -1,16 +1,16 @@
-import { assert as emberAssert } from '@ember/debug';
+import { assert } from '@ember/debug';
 import {
   getPositionDifferences,
   validateSide,
 } from 'ember-tooltips/test-support/jquery';
 
-export default function assertTooltipSpacing(assert, options) {
+export default function assertTooltipSpacing(qunitAssert, options) {
   const { side, spacing } = options;
 
   validateSide(side, 'assertTooltipSpacing');
 
   if (typeof spacing !== 'number') {
-    emberAssert(`You must pass spacing as a number like assertTooltipSpacing(assert, { side: 'top', spacing: 10 });`);
+    assert(`You must pass spacing as a number like assertTooltipSpacing(assert, { side: 'top', spacing: 10 });`);
   }
 
   const { expectedGreaterDistance, expectedLesserDistance } = getPositionDifferences(options);
@@ -24,7 +24,7 @@ export default function assertTooltipSpacing(assert, options) {
   const isSideCorrect = expectedGreaterDistance > expectedLesserDistance;
   const isSpacingCorrect = actualSpacing === spacing;
 
-  assert.ok(isSideCorrect && isSpacingCorrect,
+  qunitAssert.ok(isSideCorrect && isSpacingCorrect,
     `assertTooltipSpacing(): the tooltip should be in the correct position:
         - Tooltip should be on the ${side} side of the target: ${isSideCorrect}.
         - On the ${side} side of the target, the tooltip should be ${spacing}px from the target but it was ${actualSpacing}px`);
